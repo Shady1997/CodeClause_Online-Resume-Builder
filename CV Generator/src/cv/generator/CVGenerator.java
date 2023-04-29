575bdeacecc8c9ab30fad63eececaa3e6ed5017b
Shady Ahmed Mohamed <shadyahmed9719@gmail.com>
2023-04-29 07:56:17 -0400

    Create CV Builder Project - 29-04-2023

---
+++ CV Generator/src/cv/generator/CVGenerator.java
@@ -0,0 +1,16 @@
+package cv.generator;
+
+/**
+ *
+ * @author Shady
+ */
+public class CVGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
